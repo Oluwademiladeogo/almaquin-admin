3bf441d7241643c85b13762bfa553a37cad7a971
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-05-31 09:01:26 +0100

    update to match main almaquin repo

--- src/models/secSchool.ts
+++ src/models/secSchool.ts
@@ -1,9 +1,9 @@
 import mongoose, { Schema } from "mongoose";
-import { ISchool } from "../types/types";
+import { ISecSchool } from "../types/types";
 
-const SchoolSchema: Schema<ISchool> = new Schema({
+const SecSchoolSchema: Schema<ISecSchool> = new Schema({
   name: { type: String, required: true },
   location: { type: String },
 });
 
-export default mongoose.model<ISchool>("School", SchoolSchema);
+export default mongoose.model<ISecSchool>("School", SecSchoolSchema);
